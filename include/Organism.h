2115491939996381a20d2d89cc2c068c20d389f9
mkalejta <kalejtamikolaj@wp.pl>
2025-04-16 15:03:47 +0200

    Podpunkt 1 NF

--- include/Organism.h
+++ include/Organism.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <utility>
+#include <vector>
 #include "Position.h"
 
 using namespace std;
@@ -10,6 +12,8 @@ private:
 	int power;
 	Position position;
 	string species;
+protected:
+	vector<pair<int, int>> ancestors; // (birth_turn, death_turn)
 public:
 	Organism(int power, Position position);
 	Organism() : power(0), position(0, 0), species("O") {};
@@ -20,6 +24,8 @@ public:
 	void setPosition(Position position);
 	string getSpecies();
 	void setSpecies(string spec);
+
+	void addAncestor(int birthTurn, int deathTurn);
 
 	string toString();
 
--- src/Organism.cpp
+++ src/Organism.cpp
@@ -7,6 +7,39 @@ Organism::Organism(int power, Position position)
 	setSpecies("O");
 }
 
+// Konstruktor kopiujący
+Organism::Organism(const Organism& other)
+    : power(other.power), position(other.position), species(other.species), ancestors(other.ancestors)
+{}
+
+// Konstruktor przenoszący
+Organism::Organism(Organism&& other) noexcept
+    : power(std::move(other.power)), position(std::move(other.position)),
+	species(std::move(other.species)), ancestors(std::move(other.ancestors))
+{}
+
+// Operator przypisania kopiujący
+Organism& Organism::operator=(const Organism& other) {
+    if (this != &other) {
+        power = other.power;
+		position = other.position;
+		species = other.species;
+        ancestors = other.ancestors;
+    }
+    return *this;
+}
+
+// Operator przypisania przenoszący
+Organism& Organism::operator=(Organism&& other) noexcept {
+    if (this != &other) {
+        power = std::move(other.power);
+		position = std::move(other.position);
+		species = std::move(other.species);
+        ancestors = std::move(other.ancestors);
+    }
+    return *this;
+}
+
 int Organism::getPower()
 {
 	return this->power;
@@ -47,4 +80,8 @@ string Organism::getSpecies()
 void Organism::setSpecies(string spec)
 {
 	this->species = spec;
+}
+
+void Organism::addAncestor(int birthTurn, int deathTurn) {
+    ancestors.emplace_back(birthTurn, deathTurn);
 }
